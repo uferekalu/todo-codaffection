4c130c701434bc1820e77bf9e85c8905c902a4d4
Ufere Kalu <dekalusha@gmail.com>
2021-01-07 18:32:46 -0600

    todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todoList: []
+  }
+
+  render (){
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid py-2">
+          <div className="container">
+            <h1 className="display-2">Todos App</h1>
+          </div>
+        </div>
+        <form className="mb-3" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <input type="text" name="todoTask" className="form-control" placeholder="Please enter your task"
+              autoComplete="off" />
+              <div className="input-group-append">
+                <button type="submit" className="btn btn-outline-success">Add</button>
+              </div>
+          </div>
+        </form>
+        
+        <ul className="list-group">
+          {
+            this.state.todoList.map((item, index) => {
+              return <li className="list-group-item" key={index}>
+                {item}
+                <button className="btn btn-sm btn-outline-danger float-right" onClick={(event) => {this.deleteTodoTask(event, index)}}>Delete</button>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+  handleSubmit = (event) => {
+    var taskDesc = event.target.elements.todoTask.value;
+    if (taskDesc.length > 0) {
+      this.setState({
+        todoList: [...this.state.todoList, taskDesc]
+      })
+      event.target.reset();
+    }
+    event.preventDefault();
+  }
+
+  deleteTodoTask = (event, index) => {
+    var taskArray = [...this.state.todoList]
+    taskArray.splice(index, 1)
+    this.setState({todoList: taskArray})
+  }
 }
+
 
 export default App;
